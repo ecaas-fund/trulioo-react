c9aaeec5a725666e8d1e54a5e7c677c80c5d1ba0
Menelaos Kotsollaris <menelaos.kotsollaris@trulioo.com>
2019-01-28 14:09:29 -0800

    update .gitignore

---
+++ examples/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+import EmbedID from '../../src/EmbedID'
+
+const handleResponse = (e) => {
+    console.log('Client Recieved Response: ', e)
+}
+
+render(<EmbedID url='http://localhost:3111' handleResponse={handleResponse} />, document.getElementById("root"))
