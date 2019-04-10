5063dcbac761a18a48b8b73512483521e1ff16b9
Daniel Jordan <daniel.jordan@trulioo.com>
2019-04-10 09:51:19 -0700

    updating integration tests

--- src/tests/TruliooForm.int.test.js
+++ src/tests/TruliooForm.int.test.js
@@ -1,19 +1,18 @@
 import axios from 'axios';
 
-it('makes requests to backend', () => {
-    return expect(requestCountries()).resolves.toBeTruthy()
+it('makes requests to backend', async () => {
+    expect.assertions(1);
+    const response = await requestCountries()
+    expect(response).toBeTruthy()
 })
 
-const requestCountries = () => {
-    return new Promise((resolve, reject) => {
-        const BASE_URL = "http://localhost:3111"
-        const URL = `${BASE_URL}/api/countryCodes`
-        axios.get(URL)
-        .then((response) => {
-            resolve(response)
-        })
-        .catch((error) => {
-            reject(error)
-        });
-    })
+const requestCountries = async () => {
+    const BASE_URL = "http://localhost:3111"
+    const URL = `${BASE_URL}/api/countryCodes`
+    try {
+        let response = await axios.get(URL)
+        return response
+    } catch(err) {
+        return false
+    }
 }
