fd7a045f44ea3b7e0486b20c311fb5e7a5935a8f
Daniel Jordan <daniel.jordan@trulioo.com>
2019-04-15 15:23:26 -0700

    cleanup

--- src/tests/TruliooForm.snapshot.test.js
+++ src/tests/TruliooForm.snapshot.test.js
@@ -6,5 +6,3 @@ it('EmbedID did not change', () => {
   const embedID = renderer.create(<EmbedID url='http://localhost:3111' handleResponse={(e) => { }} />).toJSON();
   expect(embedID).toMatchSnapshot();
 });
-
-
