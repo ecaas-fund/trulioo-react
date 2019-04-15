5782025ab8abd26eaf579b272449e01a9ba4151c
mkotsollaris <mkotsollari@gmail.com>
2019-04-15 10:08:57 -0700

    eslint refactoring

--- src/EmbedID.jsx
+++ src/EmbedID.jsx
@@ -2,19 +2,32 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
-import App from './App';
+import PropTypes from 'prop-types';
 import reducers from './reducers';
+import TruliooForm from './components/TruliooForm';
 
+/* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducers,
   composeEnhancers(applyMiddleware(reduxThunk)),
 );
+/* eslint-enable */
 
-export default function EmbedID(props) {
+export default function EmbedID({ handleResponse, url }) {
   return (
     <Provider store={store}>
-      <App handleResponse={props.handleResponse} url={props.url} />
+      <TruliooForm handleResponse={handleResponse} url={url} />
     </Provider>
   );
 }
+
+EmbedID.propTypes = {
+  handleResponse: PropTypes.func,
+  url: PropTypes.string,
+};
+
+EmbedID.defaultProps = {
+  handleResponse: false,
+  url: true,
+};
