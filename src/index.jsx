f1b22a1d9cc5b148a1bcde3059295ce313234437
mkotsollaris <mkotsollari@gmail.com>
2019-10-01 23:17:53 -0700

    transpiling babel content to ./dist

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
-import App from './App';
-import reducers from './reducers';
 
-const store = createStore(
-  reducers, applyMiddleware(reduxThunk),
-);
+import EmbedID from './EmbedID';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, document.getElementById('root'),
-);
+export { EmbedID };
 
-serviceWorker.unregister();
+export default EmbedID;
