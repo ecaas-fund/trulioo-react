2ddac8b7348f352e5661437152a8a0ad619517c2
mkotsollaris <mkotsollari@gmail.com>
2019-10-15 19:21:55 -0700

    save progress

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,8 @@
 import EmbedID from './EmbedID';
 
-// eslint-disable-next-line import/prefer-default-export
+// // eslint-disable-next-line import/prefer-default-export
+// export { EmbedID };
+// module.exports = EmbedID;
+
 export { EmbedID };
+export default EmbedID;
