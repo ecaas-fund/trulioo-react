a570e8f9771337889601cd4c083e176cc303bc6c
mkotsollaris <mkotsollari@gmail.com>
2019-10-15 23:56:17 -0700

    updated prepublised to prepare

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,4 @@
 import EmbedID from './EmbedID';
 
-// // eslint-disable-next-line import/prefer-default-export
-// export { EmbedID };
-// module.exports = EmbedID;
-
 export { EmbedID };
 export default EmbedID;
